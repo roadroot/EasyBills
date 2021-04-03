07fe504924d2ded89d719aeb7e61b07640faf8d7
roadroot <mohamed.azikiou@gmail.com>
2021-04-03 20:07:35 +0200

    All

--- src/app/fade.animation.ts
+++ src/app/fade.animation.ts
@@ -1,7 +1,14 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
 export let fade = trigger('fade', [
-  state('hidden', style({ opacity: 0 })),
+  state('open', style({ opacity: 1 })),
+  state('close', style({ opacity: 0 })),
   transition(':enter, :leave', [
+    animate(4000)
+  ]),
+  transition('open, close', [
+    animate(4000)
+  ]),
+  transition('close, open', [
     animate(4000)
   ])
 ]);
