98fff5638b807f9898d2ba5df61fe642dae978d4
roadroot <mohamed.azikiou@gmail.com>
2021-04-04 11:58:21 +0200

    All

---
+++ src/app/statistics/statistics.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss']
+})
+export class StatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
